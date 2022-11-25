12101af9c676859d6972a9da0bd179e9db178585
LucioFurnari <luciofurnari99@gmail.com>
2022-11-24 21:32:09 -0300

    Change Main function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,7 @@
 
 export function Main() {
-    const main = document.createElement("div");
+    const mainContainer = document.querySelector("#content");
     const textTest = document.createElement("p");
     textTest.textContent = "Test";
-    main.classList.add("main");
-    main.appendChild(textTest);
-    return main
+    mainContainer.appendChild(textTest);
 }
