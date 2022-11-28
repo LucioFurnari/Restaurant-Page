59536c66ed118f5a879c8351627f7fbb990debff
LucioFurnari <luciofurnari99@gmail.com>
2022-11-27 23:53:47 -0300

    Add #content element selector, tabbedBrowsing function and add events to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import { Main } from "./components/Main";
+import { homeComponent } from "./components/Home";
+import { contactComponent } from "./components/Contact";
+import { headerComponent } from "./components/Header";
 import "./styles/main.css"
 import "./styles/header.css"
 import "./styles/home.css"
+import "./styles/contact.css"
 
-Main();
+const mainContainer = document.querySelector("#content");
+mainContainer.appendChild(headerComponent())
+mainContainer.appendChild(homeComponent())
+
+const homeBtn = document.querySelector(".home-button");
+const menuBtn = document.querySelector(".menu-button");
+const aboutBtn = document.querySelector(".about-button");
+
+function tabbedBrowsing(component) {
+    mainContainer.removeChild(mainContainer.children[1]);
+    mainContainer.appendChild(component)
+}
+
+aboutBtn.addEventListener("click",()=> tabbedBrowsing(contactComponent()))
+homeBtn.addEventListener("click",() => tabbedBrowsing(homeComponent()))
